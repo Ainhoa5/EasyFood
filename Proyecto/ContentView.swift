95ef5303407b284db81a171376cb097013564667
Your Name <you@example.com>
2023-02-17 10:04:53 +0000

    first commit

---
+++ Proyecto/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Proyecto
+//
+//  Created by CIFP Villa De Aguimes on 16/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, Taylor Swift!")
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            };
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Proyecto/ProyectoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoApp.swift
+//  Proyecto
+//
+//  Created by CIFP Villa De Aguimes on 16/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
